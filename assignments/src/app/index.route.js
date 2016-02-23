8e7f087b144e6605d924fe9cf003668ab12c4621
shraddhakurhade <s.kurhade@globant.com>
2016-02-24 02:27:09 +0530

    Update index.route.js

--- assignments/src/app/index.route.js
+++ assignments/src/app/index.route.js
@@ -12,7 +12,38 @@
         templateUrl: 'app/views/dashboard.html',
         controller: 'DashboardController',
         controllerAs: 'dash'
-      });
+      })
+	  .state('setting', {
+        url: '/setting',
+        templateUrl: 'app/views/setting.html',
+        controller: 'RecurringController',
+        controllerAs: 'recur'
+      })
+	.state('expense', {
+			url: '/expense',
+			templateUrl: 'app/views/expense.html',
+			controller: 'DashboardController',
+			controllerAs: 'dash'
+		 })
+	.state('income', {
+			url: '/income',
+			templateUrl: 'app/views/income.html',
+			controller: 'DashboardController',
+			controllerAs: 'dash'
+		 })
+	  .state('transaction', {
+			url: '/transaction',
+			templateUrl: 'app/views/transaction.html',
+			controller: 'DashboardController',
+			controllerAs: 'dash'
+		 })
+	.state('report', {
+			url: '/report',
+			templateUrl: 'app/views/report.html',
+			controller: 'DashboardController',
+			controllerAs: 'dash'
+		 })
+	
     /*.state('dashboard.income', {
 		parent: 'dashboard',
         url: '/income',
@@ -28,7 +59,7 @@
         controllerAs: 'dash'
       });*/
      //$urlRouterProvider.otherwise('/transaction');
-     $urlRouterProvider.otherwise('/dashboard');
+     $urlRouterProvider.otherwise('/expense');
 
   }   
 })();
